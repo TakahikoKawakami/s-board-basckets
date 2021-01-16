b78c12b94924a223fbab048e631c8743ff8b4817
TakahikoKawakami <takahiko.kawakami1128@gmail.com>
2021-01-16 20:44:54 +0900

    - arrange files

--- .gitpod-init.sh
+++
@@ -1,3 +0,0 @@
-mysql -u root -e "create database basket_analysis"
-mysql -u root -e "alter user root@localhost identified by 'root'"
-cp src/.env.example src/.env
--- .gitpod.Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM gitpod/workspace-mysql
-USER root
-# RUN sudo apt-get update -q \
-#     && sudo apt-get install -y php-dev
