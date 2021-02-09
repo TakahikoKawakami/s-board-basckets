a6ab768efbe1a626fd9dd68874e4fac29eec9e68
shabaraba <work.shabaraba@gmail.com>
2021-02-09 09:39:51 +0900

    - can run server for prod

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 import uvicorn
 
+from app.config import AppConfig
+from app.application_responder import api
+
 """Webサーバを立ち上げる際に実行するファイル"""
 if __name__ == "__main__":
-    # api.run(address="0.0.0.0", port=5500, debug=True)
-    # uvicorn.run("run:api", host='0.0.0.0', log_config="logging_config.json", port=5500, debug=True, reload=True)
-    uvicorn.run(
-        "app.application_responder:api", 
-        host='0.0.0.0', 
-        port=5500, 
-        debug=True, 
-        reload=True,
-        reload_dirs=['app']
-    )
+    if AppConfig.ENV_DIVISION == AppConfig.ENV_DIVISION_PRODUCTION:
+        api.run(port=5500, debug=False)
+    else:
+        # uvicorn.run("run:api", host='0.0.0.0', log_config="logging_config.json", port=5500, debug=True, reload=True)
+        uvicorn.run(
+            "app.application_responder:api", 
+            host='0.0.0.0', 
+            port=5500, 
+            debug=True, 
+            reload=True,
+            reload_dirs=['app']
+        )
