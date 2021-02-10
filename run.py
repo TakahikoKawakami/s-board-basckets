edb9591af3146a3f3eb205cdf3a17838fc64f495
shabaraba <work.shabaraba@gmail.com>
2021-02-10 11:16:05 +0900

    tmp

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if AppConfig.ENV_DIVISION == AppConfig.ENV_DIVISION_PRODUCTION:
         api.run(port=1000, debug=False)
     elif AppConfig.ENV_DIVISION == AppConfig.ENV_DIVISION_STAGING:
-        api.run(port=2000, debug=True)
+        api.run(port=6500, debug=True)
     else:
         # uvicorn.run("run:api", host='0.0.0.0', log_config="logging_config.json", port=5500, debug=True, reload=True)
         uvicorn.run(
