fcd3d25801942ed81d6feb47a1c3d9206b00e768
shabaraba <work.shabaraba@gmail.com>
2021-04-11 13:41:06 +0900

    - docker環境構築

--- app/common/abstracts/AbstractTortoiseModel.py
+++ app/common/abstracts/AbstractTortoiseModel.py
@@ -1,4 +1,5 @@
-from tortoise import Model, fields
+from tortoise.models import Model
+from tortoise import fields
 from tortoise.contrib.pydantic import pydantic_model_creator, pydantic_queryset_creator
 
 from datetime import datetime
